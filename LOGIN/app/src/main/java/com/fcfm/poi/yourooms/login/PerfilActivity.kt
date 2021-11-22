0c8b51194acada1ff50bd5d761c10a01701faa6c
Bryan Duarte <stfrisephone@gmail.com>
2021-11-21 22:33:41 -0600

    Load user data on profile activity

--- LOGIN/app/src/main/java/com/fcfm/poi/yourooms/login/PerfilActivity.kt
+++ LOGIN/app/src/main/java/com/fcfm/poi/yourooms/login/PerfilActivity.kt
@@ -1,5 +1,6 @@
 package com.fcfm.poi.yourooms.login
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -7,13 +8,21 @@ import android.widget.Toast
 import android.view.inputmethod.InputBinding
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.fcfm.poi.yourooms.login.authentication.AuthenticationManager
+import com.fcfm.poi.yourooms.login.data.models.dao.UserDao
 import com.fcfm.poi.yourooms.login.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PerfilActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pantalla_perfil)
+
+        loadUserData()
 
         val spinner = findViewById<Spinner>(R.id.spinner_estado)
         val lista = resources.getStringArray(R.array.estados_usuario)
@@ -38,7 +47,19 @@ class PerfilActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun loadUserData() {
+        val userId = AuthenticationManager().getCurrentUser()?.uid ?: return
 
+        CoroutineScope(Dispatchers.IO).launch {
+            val userModel = UserDao().getUser(userId)
+
+            if (userModel != null) {
+                withContext(Dispatchers.Main) {
+                    findViewById<TextView>(R.id.textView_Nombre_Perfil).text = "${userModel.name} ${userModel.lastname}"
+                    findViewById<TextView>(R.id.textView_Correo).text = userModel.email
+                }
+            }
+        }
     }
 }
