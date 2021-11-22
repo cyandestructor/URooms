ccb48e5f36defc49eec28055c91c0f4a693282ad
Bryan Duarte <stfrisephone@gmail.com>
2021-11-22 06:11:40 -0600

    Added images to the chats and members

--- LOGIN/app/src/main/java/com/fcfm/poi/yourooms/login/adapters/MemberListAdapter.kt
+++ LOGIN/app/src/main/java/com/fcfm/poi/yourooms/login/adapters/MemberListAdapter.kt
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.fcfm.poi.yourooms.login.R
 import com.fcfm.poi.yourooms.login.data.models.User
+import com.squareup.picasso.Picasso
 
 class MemberListAdapter(private val members : List<User>) : RecyclerView.Adapter<MemberListAdapter.MemberHolder>(), View.OnClickListener {
     private var listener: View.OnClickListener? = null
@@ -16,6 +18,13 @@ class MemberListAdapter(private val members : List<User>) : RecyclerView.Adapter
         @SuppressLint("SetTextI18n")
         fun bind(member : User) {
             itemView.findViewById<TextView>(R.id.titulo_nombre).text = "${member.name} ${member.lastname}"
+            val imageView = itemView.findViewById<ImageView>(R.id.icono_pic_grupo)
+            Picasso.get()
+                .load(member.image)
+                .placeholder(R.drawable.avatar_placeholder)
+                .error(R.drawable.avatar_placeholder)
+                .into(imageView)
+            itemView.findViewById<TextView>(R.id.estadoConexion).text = member.connectionState ?: "Desconocido"
             itemView.tag = member
         }
     }
